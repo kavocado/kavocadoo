06f4ee3c61db47834085b4e78cc67d46637b4626
kavocado <88789704+kavocado@users.noreply.github.com>
2021-08-12 00:06:28 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "How u doing",
+  text2: "I have a question for you, bitch.",
+  text3: "Am I cute",
+  text4: "Nếu mày ko trả lời mà thoát ra thì mày là chos ",
+  text5: "U là tr, có điên khum",
+  text6: "Obviously",
+  text7: "Why am I so cute",
+  text8: "Send to me <3",
+  text9: "You are so fucking cute",
+  text10: "I know hehee",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Tối nay nổ inbox nghe",
+  text12: "OKrrrr",
 };
 
 $(document).ready(function () {
